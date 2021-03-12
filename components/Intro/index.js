9b907662c66efa6794c759e47fcc06aa266596af
Megan Walter <megan@bighuman.com>
2021-03-11 20:26:41 -0500

    initial scaffolding up

---
+++ components/Intro/index.js
@@ -0,0 +1,8 @@
+import styles from './Intro.module.css'
+export default function Intro() {
+  return (
+    <p className={styles.description}>
+      Hospitality major turned developer &#128187;, living in the greatest city on earth. Currently enjoying digital nomad life &#128656; you can probably find me in the desert &#127964;, on the PCH &#127754; or among the trees somewhere &#127795;.
+    </p>
+  )
+}
